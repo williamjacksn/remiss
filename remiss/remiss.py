7838ea33012e623200745387eaebb66dad9e61e1
William Jackson <william@subtlecoolness.com>
2016-02-19 11:00:49 -0600

    Update remiss.py

--- remiss/remiss.py
+++ remiss/remiss.py
@@ -1,10 +1,18 @@
 import flask
+import os
+
+SLACK_TEAM_NAME = os.environ.get('SLACK_TEAM_NAME')
+SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'This is the index.'
+    if SLACK_TEAM_NAME is None:
+        return 'You need to set the \'SLACK_TEAM_NAME\' environment variable.'
+    if SLACK_TOKEN is None:
+        return 'You need to set the \'SLACK_TOKEN\' environment variable.'
+    return 'https://{}.slack.com/api/users.admin.invite'.format(SLACK_TEAM_NAME)
 
 def main():
     app.run()
